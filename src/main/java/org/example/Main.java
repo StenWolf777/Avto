33602ceafb1e1a38c6a08fb76d1f851e633c70c8
StepenWolf777 <ae5485489@gmail.com>
2024-10-07 15:19:12 +0500

    updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             System.out.println("Ok");
+
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(new MyBot("6996741296:AAFtaKOst5LabTcUnrfVRXEwp299J5O2dTc"));
             System.out.println(1);
